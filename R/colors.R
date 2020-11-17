c227bfce563caea6c8e839374d786edc3cf1bd51
rmvpaeme <45523344+rmvpaeme@users.noreply.github.com>
2020-11-17 17:20:33 +0100

    Update colors.R

--- R/colors.R
+++ R/colors.R
@@ -1,45 +1,29 @@
 #' Complete list of palettes
 #'
-#' Use \code{\link{wes_palette}} to construct palettes of desired length.
+#' Use \code{\link{qg_palette}} to construct palettes of desired length.
 #'
 #' @export
-wes_palettes <- list(
-  BottleRocket1 = c("#A42820", "#5F5647", "#9B110E", "#3F5151", "#4E2A1E", "#550307", "#0C1707"),
-  BottleRocket2 = c("#FAD510", "#CB2314", "#273046", "#354823", "#1E1E1E"),
-  Rushmore1 = c("#E1BD6D", "#EABE94", "#0B775E", "#35274A" ,"#F2300F"),
-  Rushmore = c("#E1BD6D", "#EABE94", "#0B775E", "#35274A" ,"#F2300F"),
-  Royal1 = c("#899DA4", "#C93312", "#FAEFD1", "#DC863B"),
-  Royal2 = c("#9A8822", "#F5CDB4", "#F8AFA8", "#FDDDA0", "#74A089"),
-  Zissou1 = c("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"),
-  Darjeeling1 = c("#FF0000", "#00A08A", "#F2AD00", "#F98400", "#5BBCD6"),
-  Darjeeling2 = c("#ECCBAE", "#046C9A", "#D69C4E", "#ABDDDE", "#000000"),
-  Chevalier1 = c("#446455", "#FDD262", "#D3DDDC", "#C7B19C"),
-  FantasticFox1 = c("#DD8D29", "#E2D200", "#46ACC8", "#E58601", "#B40F20"),
-  Moonrise1 = c("#F3DF6C", "#CEAB07", "#D5D5D3", "#24281A"),
-  Moonrise2 = c("#798E87", "#C27D38", "#CCC591", "#29211F"),
-  Moonrise3 = c("#85D4E3", "#F4B5BD", "#9C964A", "#CDC08C", "#FAD77B"),
-  Cavalcanti1 = c("#D8B70A", "#02401B", "#A2A475", "#81A88D", "#972D15"),
-  GrandBudapest1 = c("#F1BB7B", "#FD6467", "#5B1A18", "#D67236"),
-  GrandBudapest2 = c("#E6A0C4", "#C6CDF7", "#D8A499", "#7294D4"),
-  IsleofDogs1 = c("#9986A5", "#79402E", "#CCBA72", "#0F0D0E", "#D9D0D3", "#8D8680"),
-  IsleofDogs2 = c("#EAD3BF", "#AA9486", "#B6854D", "#39312F", "#1C1718")
+qg_palettes <- list(
+  openings = c("#191a1e", "#765f4f", "#7e5036", "#6e7070"),
+  adopted = c("#4e2f26", "#12202c", "#59757e", "#949ea2", "#23413f"),
+  moscow = c("#7a534c", "#002629", "#a59b92"),
+  endgame = c("#091113", "#d8e2e4", "#6b5756", "#3a2426"),
+  practice = c("#997a80", "#9daca7", "#322920", "#3d515a"),
+  room = c("#9e9693", "#65403a", "#75574d", "#2f292b"),
+  squash = c("#0a0a0a", "#8f9190", "#5c4943", "#532623",  "#636365"),
+  USopen = c("#d3a0a5", "#6e4030", "#3b5963", "#636d64"),
 )
 
-#' A Wes Anderson palette generator
+#' A Queen's Gambit palette generator
 #'
-#' These are a handful of color palettes from Wes Anderson movies.
-#'
+#' These are a handful of color palettes from the Netflix series "The Queens Gambit".
+#' Inspired by the \href{https://github.com/karthik/wesanderson}{Wes Anderson Palettes}.
 #' @param n Number of colors desired. Unfortunately most palettes now only
-#'   have 4 or 5 colors. But hopefully we'll add more palettes soon. All color
-#'   schemes are derived from the most excellent Tumblr blog:
-#'   \href{http://wesandersonpalettes.tumblr.com/}{Wes Anderson Palettes}.
-#'   If omitted, uses all colours.
+#'   have 3 or 5 colors. If omitted, uses all colours.
 #' @param name Name of desired palette. Choices are:
-#'   \code{BottleRocket1}, \code{BottleRocket2},  \code{Rushmore1},
-#'   \code{Royal1}, \code{Royal2},  \code{Zissou1}, \code{Darjeeling1},
-#'   \code{Darjeeling2},  \code{Chevalier1} , \code{FantasticFox1} ,
-#'   \code{Moonrise1}, \code{Moonrise2}, \code{Moonrise3}, \code{Cavalcanti1},
-#'   \code{GrandBudapest1}, \code{GrandBudapest2}, \code{IsleofDogs1}, \code{IsleofDogs2}
+#'   \code{adopted}, \code{endgame},  \code{moscow},
+#'   \code{openings}, \code{practice},  \code{room}, \code{squash},
+#'   \code{USopen}
 #' @param type Either "continuous" or "discrete". Use continuous if you want
 #'   to automatically interpolate between colours.
 #'   @importFrom graphics rgb rect par image text
@@ -47,20 +31,18 @@ wes_palettes <- list(
 #' @export
 #' @keywords colors
 #' @examples
-#' wes_palette("Royal1")
-#' wes_palette("GrandBudapest1")
-#' wes_palette("Cavalcanti1")
-#' wes_palette("Cavalcanti1", 3)
+#' qg_palette("adopted")
+#' qg_palette("moscow")
 #'
 #' # If you need more colours than normally found in a palette, you
 #' # can use a continuous palette to interpolate between existing
 #' # colours
-#' pal <- wes_palette(21, name = "Zissou1", type = "continuous")
+#' pal <- qg_palette(21, name = "room", type = "continuous")
 #' image(volcano, col = pal)
-wes_palette <- function(name, n, type = c("discrete", "continuous")) {
+qg_palette <- function(name, n, type = c("discrete", "continuous")) {
   type <- match.arg(type)
 
-  pal <- wes_palettes[[name]]
+  pal <- qg_palettes[[name]]
   if (is.null(pal))
     stop("Palette not found.")
 
